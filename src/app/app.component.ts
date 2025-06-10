e4449350fbfee5bee61ff3bfe7902d6f8a517af5
iamernesto14 <eernesto211@gmail.com>
2025-06-10 12:04:55 +0000

    Add cart, order-confirmation, product-card component and services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+// import { AddToCartComponent } from "./components/add-to-cart/add-to-cart.component";
+import { ProductCardComponent } from './components/product-card/product-card.component';
+import { CartComponent } from './components/cart/cart.component';
 import { AddToCartComponent } from "./components/add-to-cart/add-to-cart.component";
 
 
 @Component({
   selector: 'app-root',
-  imports: [AddToCartComponent],
+  imports: [ProductCardComponent, CartComponent, AddToCartComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/components/product-card/product-card.component.ts
+++ src/app/components/product-card/product-card.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { AddToCartComponent } from '../add-to-cart/add-to-cart.component';
 
 @Component({
   selector: 'app-product-card',
-  imports: [],
+  imports: [AddToCartComponent],
   templateUrl: './product-card.component.html',
   styleUrl: './product-card.component.scss'
 })
